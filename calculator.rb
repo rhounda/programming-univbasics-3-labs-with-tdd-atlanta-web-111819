80561129fe411f394c2783eca8db64cde4311dd3
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-02 00:49:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@
 first_number = 10
 second_number = 20
 sum = first_number + second_number
-subtracting = first_number - second_number
+difference = first_number - second_number
 product = first_number * second_number
-qutient = first_number  second_number
+qutient = first_number / second_numbe

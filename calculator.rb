970275cae07a5ad6f1b4ab59e8415ffcde0b71ba
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-02 00:52:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here 
-first_number = 10
-second_number = 20
+first_number = 20
+second_number = 10
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number

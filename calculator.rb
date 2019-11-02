c31a1e56f77d5f1ba93a1c8403d62bfa70a8d104
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-02 00:46:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,6 @@
 first_number = 10
 second_number = 20
 sum = first_number + second_number
-sum = first_number + second_number
-sum = first_number + second_number
+sum = first_number - second_number
+sum = first_number * second_number
+sum = first_number % second_number
